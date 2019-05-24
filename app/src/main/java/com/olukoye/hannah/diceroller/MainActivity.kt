0dc2ce388b290247ef18e7e94f43d851b893bc00
hannaholukoye <hannaholukoye@gmail.com>
2019-05-24 22:51:18 +0300

    Feat: Use dice images for random number

--- app/src/main/java/com/olukoye/hannah/diceroller/MainActivity.kt
+++ app/src/main/java/com/olukoye/hannah/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.olukoye.hannah.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -23,8 +24,19 @@ class MainActivity : AppCompatActivity() {
      fun rollDice() {
          val randomInt = Random().nextInt(6) + 1
 
-         val resultText: TextView = findViewById(R.id.result_text)
-         resultText.text = randomInt.toString()
+         val diceImage: ImageView = findViewById(R.id.dice_image)
+
+         val drawableResource = when (randomInt) {
+             1 -> R.drawable.dice_1
+             2 -> R.drawable.dice_2
+             3 -> R.drawable.dice_3
+             4 -> R.drawable.dice_4
+             5 -> R.drawable.dice_5
+             else -> R.drawable.dice_6
+         }
+
+         diceImage.setImageResource(drawableResource)
+
 
 
      }
